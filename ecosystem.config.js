bb0ea4ee7513133cefbc89fc7d35569991cd8bc9
0strich <gee8195@naver.com>
2023-05-10 12:59:19 +0900

    add files

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "node",
+      script: "npm",
+      args: "start",
+      watch: true,
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+    },
+  ],
+};
